04bc6744e9a23247dc1ba7774199fd46c8c25b86
Garry_Lv <svyatogavr@yandex.ru>
2021-01-18 18:12:41 +0300

    Litle changes

--- test2.py
+++ test2.py
@@ -12,19 +12,35 @@ def load_image(name, color_key=None):
         raise SystemExit(message)
     image = image.convert_alpha()
     if color_key is not None:
-        if color_key == -1:
+        if color_key is -1:
             color_key = image.get_at((0, 0))
         image.set_colorkey(color_key)
     return image
 
 
+pygame.init()
+screen_size = (500, 500)
+screen = pygame.display.set_mode(screen_size)
+FPS = 50
+
+tile_images = {
+    'wall': load_image('box.png'),
+    'empty': load_image('grass.png')
+}
+player_image = load_image('hero.png')
+
+tile_width = tile_height = 50
+
+
 class ScreenFrame(pygame.sprite.Sprite):
+
     def __init__(self):
         super().__init__()
         self.rect = (0, 0, 500, 500)
 
 
 class SpriteGroup(pygame.sprite.Group):
+
     def __init__(self):
         super().__init__()
 
@@ -52,17 +68,19 @@ class Tile(Sprite):
 
 
 class Player(Sprite):
-    def __init__(self, pos_x, pos_y):
+    def __init__(self, pos_x, pos_y, a=0, b=0):
         super().__init__(hero_group)
         self.image = player_image
         self.rect = self.image.get_rect().move(
-            tile_width * pos_x + 15, tile_height * pos_y + 5)
+            tile_width * pos_x + a, tile_height * pos_y + b)
         self.pos = (pos_x, pos_y)
+        self.a=a
+        self.b=b
 
     def move(self, x, y):
         self.pos = (x, y)
         self.rect = self.image.get_rect().move(
-            tile_width * self.pos[0] + 15, tile_height * self.pos[1] + 5)
+            tile_width * self.pos[0] + self.a, tile_height * self.pos[1] + self.b)
 
 
 player = None
@@ -102,7 +120,6 @@ def start_screen():
             elif event.type == pygame.KEYDOWN or \
                     event.type == pygame.MOUSEBUTTONDOWN:
                 return
-
         pygame.display.flip()
         clock.tick(FPS)
 
@@ -150,19 +167,6 @@ def move(hero, movement):
         if x < max_x - 1 and level_map[y][x + 1] == ".":
             hero.move(x + 1, y)
 
-
-pygame.init()
-screen_size = (500, 500)
-screen = pygame.display.set_mode(screen_size)
-FPS = 50
-
-tile_images = {
-    'wall': load_image('box.png'),
-    'empty': load_image('grass.png')
-}
-player_image = load_image('hero.png')
-
-tile_width = tile_height = 50
 
 start_screen()
 level_map = load_level("map.map")
